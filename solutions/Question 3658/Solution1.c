54d39bc3b64f893f2b264ad18f3acd505a6b3376
Siddhant Singh Rawat <rawatsiddhant25@gmail.com>
2026-09-09 22:59:41 +0530

    feat(Question 3658): add c solution (Solution1.c) for GCD of Odd and Even Sums [Easy]

---
+++ solutions/Question 3658/Solution1.c
@@ -0,0 +1,14 @@
+/*
+ * Problem: 3658. GCD of Odd and Even Sums
+ * Difficulty: Easy
+ * Topics: Math, Number Theory
+ * URL: https://leetcode.com/problems/gcd-of-odd-and-even-sums/
+ * Runtime: 2 ms
+ * Memory: 9.1 MB
+ * Solved via CodePath Auto-Committer
+ */
+
+
+int gcdOfOddEvenSums(int n) {
+    return n;
+}
