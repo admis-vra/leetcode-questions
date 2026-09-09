91a2a6712b7faf15c0f9de5a954f6e7ddd69e6b5
Siddhant Singh Rawat <rawatsiddhant25@gmail.com>
2026-09-09 23:01:37 +0530

    feat(Question 812): add python3 solution (Solution2.py) for Largest Triangle Area [Easy]

---
+++ solutions/Question 812/Solution2.py
@@ -0,0 +1,20 @@
+# Problem: 812. Largest Triangle Area
+# Difficulty: Easy
+# Topics: Array, Math, Geometry, Polygons
+# URL: https://leetcode.com/problems/largest-triangle-area/
+# Runtime: 48 ms
+# Memory: 17.6 MB
+# Solved via CodePath Auto-Committer
+
+class Solution:
+    def largestTriangleArea(self, points: List[List[int]]) -> float:
+        ans = 0
+        for i in range(len(points)):
+            for j in range(i+1, len(points)):
+                for k in range(j+1, len(points)):
+                    x1, y1 = points[i]
+                    x2, y2 = points[j]
+                    x3, y3 = points[k]
+                    area = 0.5 * abs(x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2))
+                    ans = max(ans, area)
+        return ans
