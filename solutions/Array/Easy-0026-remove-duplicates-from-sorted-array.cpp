ea130313432a798d4d35e2fd37fedc7fbdf18864
Siddhant Singh Rawat <rawatsiddhant25@gmail.com>
2026-09-09 21:33:41 +0530

    Add accepted solution for #26: Remove Duplicates from Sorted Array [Easy]

--- solutions/Array/Easy-0026-remove-duplicates-from-sorted-array.cpp
+++ solutions/Array/Easy-0026-remove-duplicates-from-sorted-array.cpp
@@ -3,6 +3,18 @@
  * Difficulty: Easy
  * Topics: Array, Two Pointers
  * URL: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+ *
+ * Description:
+ * Given an integer array `nums` sorted in **non-decreasing order**, remove the duplicates **in-place** such that each unique element appears only **once**. The **relative order** of the elements should be kept the **same**.
+ * 
+ * Consider the number of *unique elements* in `nums` to be `k**​​​​​​​**`​​​​​​​. After removing duplicates, return the number of unique elements `k`.
+ * 
+ * The first `k` elements of `nums` should contain the unique numbers in **sorted order**. The remaining elements beyond index `k - 1` can be ignored.
+ * 
+ * **Custom Judge:**
+ * 
+ * The judge will test your solution with the following code:
+ * 
  * Solved via CodePath Auto-Committer
  */
 
