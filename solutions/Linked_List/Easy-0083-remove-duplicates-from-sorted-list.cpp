d775244040fe7aa1ab8201b863ec0ef1231353c7
Siddhant Singh Rawat <rawatsiddhant25@gmail.com>
2026-09-09 21:32:22 +0530

    Add accepted solution for #83: Remove Duplicates from Sorted List [Easy]

---
+++ solutions/Linked_List/Easy-0083-remove-duplicates-from-sorted-list.cpp
@@ -0,0 +1,38 @@
+/*
+ * Problem: 83. Remove Duplicates from Sorted List
+ * Difficulty: Easy
+ * Topics: Linked List
+ * URL: https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+ *
+ * Description:
+ * Given the `head` of a sorted linked list, *delete all duplicates such that each element appears only once*. Return *the linked list **sorted** as well*.
+ * 
+ *  
+ * 
+ * **Example 1:**
+ * 
+ * ```
+ * **Input:** head = [1,1,2]
+ * **Output:** [1,2]
+ * ```
+ * 
+ * **Example 2:**
+ * 
+ * ```
+ * **Input:** head = [1,1,2,3,3]
+ * **Output:** [1,2,3]
+ * ```
+ * 
+ *  
+ * 
+ * **Constraints:**
+ * 
+ * 	
+ * - The number of nodes in the list is in the range `[0, 300]`.
+ * 	
+ * - `-100
+ * Solved via CodePath Auto-Committer
+ */
+
+// Solution for Remove Duplicates from Sorted List
+// Solved on LeetCode
