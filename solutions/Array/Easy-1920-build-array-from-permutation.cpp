448ef52b2b2588ef4c3c9b72f65cee4db5678da4
Siddhant Singh Rawat <rawatsiddhant25@gmail.com>
2026-09-09 21:13:04 +0530

    Add accepted solution for #1920: Build Array from Permutation [Easy]

---
+++ solutions/Array/Easy-1920-build-array-from-permutation.cpp
@@ -0,0 +1,9 @@
+/*
+ * Problem: 1920. Build Array from Permutation
+ * Difficulty: Easy
+ * Topics: Array, Simulation
+ * URL: https://leetcode.com/problems/build-array-from-permutation/
+ * Solved via CodePath Auto-Committer
+ */
+
+// Code awaiting input or session cookie
