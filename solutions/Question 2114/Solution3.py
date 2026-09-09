1569e1872249b0b26b50d76b9865ee3dca295b27
Siddhant Singh Rawat <rawatsiddhant25@gmail.com>
2026-09-09 23:02:54 +0530

    feat(Question 2114): add python3 solution (Solution3.py) for Maximum Number of Words Found in Sentences [Easy]

---
+++ solutions/Question 2114/Solution3.py
@@ -0,0 +1,11 @@
+# Problem: 2114. Maximum Number of Words Found in Sentences
+# Difficulty: Easy
+# Topics: Array, String
+# URL: https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/
+# Runtime: 4 ms
+# Memory: 18 MB
+# Solved via CodePath Auto-Committer
+
+class Solution:
+    def mostWordsFound(self, sentences: List[str]) -> int:
+        return max(len(x.split()) for x in sentences)
