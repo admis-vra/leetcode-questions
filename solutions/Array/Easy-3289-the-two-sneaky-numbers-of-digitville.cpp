2f9ae1a1bcfd8a2e23ac58669edc25072d29a1ef
Siddhant Singh Rawat <rawatsiddhant25@gmail.com>
2026-09-09 21:30:38 +0530

    Add accepted solution for #3289: The Two Sneaky Numbers of Digitville [Easy]

---
+++ solutions/Array/Easy-3289-the-two-sneaky-numbers-of-digitville.cpp
@@ -0,0 +1,27 @@
+/*
+ * Problem: 3289. The Two Sneaky Numbers of Digitville
+ * Difficulty: Easy
+ * Topics: Array, Hash Table, Math
+ * URL: https://leetcode.com/problems/the-two-sneaky-numbers-of-digitville/
+ *
+ * Description:
+ * In the town of Digitville, there was a list of numbers called `nums` containing integers from `0` to `n - 1`. Each number was supposed to appear **exactly once** in the list, however, **two** mischievous numbers sneaked in an *additional time*, making the list longer than usual.
+ * 
+ * As the town detective, your task is to find these two sneaky numbers. Return an array of size **two** containing the two numbers (in *any order*), so peace can return to Digitville.
+ * 
+ *  
+ * 
+ * **Example 1:**
+ * 
+ * **Input:** nums = [0,1,1,0]
+ * 
+ * **Output:** [0,1]
+ * 
+ * **Explanation:**
+ * 
+ * The numbers 0 and 1 each appear twice in the array.
+ * Solved via CodePath Auto-Committer
+ */
+
+// Solution for The Two Sneaky Numbers of Digitville
+// Solved on LeetCode
