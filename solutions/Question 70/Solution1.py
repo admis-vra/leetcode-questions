973292de5e13f532396d6be85c55c6b2db647e02
Siddhant Singh Rawat <rawatsiddhant25@gmail.com>
2026-09-09 23:01:00 +0530

    feat(Question 70): add python3 solution (Solution1.py) for Climbing Stairs [Easy]

---
+++ solutions/Question 70/Solution1.py
@@ -0,0 +1,17 @@
+# Problem: 70. Climbing Stairs
+# Difficulty: Easy
+# Topics: Math, Dynamic Programming, Memoization
+# URL: https://leetcode.com/problems/climbing-stairs/
+# Runtime: 0 ms
+# Memory: 17.8 MB
+# Solved via CodePath Auto-Committer
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        a, b = 1, 2  
+        for _ in range(3, n+1):
+                a, b = b, a+b
+        return b
+        
