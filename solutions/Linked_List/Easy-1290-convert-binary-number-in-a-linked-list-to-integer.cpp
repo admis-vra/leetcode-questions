846c0755d2d134fdc665eca3584bc655e1e63850
Siddhant Singh Rawat <rawatsiddhant25@gmail.com>
2026-09-09 21:13:16 +0530

    Add accepted solution for #1290: Convert Binary Number in a Linked List to Integer [Easy]

---
+++ solutions/Linked_List/Easy-1290-convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -0,0 +1,9 @@
+/*
+ * Problem: 1290. Convert Binary Number in a Linked List to Integer
+ * Difficulty: Easy
+ * Topics: Linked List, Math
+ * URL: https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+ * Solved via CodePath Auto-Committer
+ */
+
+// Code awaiting input or session cookie
