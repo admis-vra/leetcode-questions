f4dda93eaa51bc14974346784027931fce6ddc16
Siddhant Singh Rawat <rawatsiddhant25@gmail.com>
2026-09-09 21:32:04 +0530

    Add accepted solution for #169: Majority Element [Easy]

---
+++ solutions/Array/Easy-0169-majority-element.cpp
@@ -0,0 +1,47 @@
+/*
+ * Problem: 169. Majority Element
+ * Difficulty: Easy
+ * Topics: Array, Hash Table, Divide and Conquer, Sorting, Counting, Boyer–Moore Majority Vote Algorithm
+ * URL: https://leetcode.com/problems/majority-element/
+ *
+ * Description:
+ * Given an array `nums` of size `n`, return *the majority element*.
+ * 
+ * The majority element is the element that appears more than `&lfloor;n / 2&rfloor;` times. You may assume that the majority element always exists in the array.
+ * 
+ *  
+ * 
+ * **Example 1:**
+ * 
+ * ```
+ * **Input:** nums = [3,2,3]
+ * **Output:** 3
+ * ```
+ * 
+ * **Example 2:**
+ * 
+ * ```
+ * **Input:** nums = [2,2,1,1,1,2,2]
+ * **Output:** 2
+ * ```
+ * 
+ *  
+ * 
+ * **Constraints:**
+ * 
+ * 	
+ * - `n == nums.length`
+ * 	
+ * - `1 4`
+ * 	
+ * - `-109 9`
+ * 	
+ * - The input is generated such that a majority element will exist in the array.
+ * 
+ *  
+ * **Follow-up:** Could you solve the problem in linear time and in `O(1)` space?
+ * Solved via CodePath Auto-Committer
+ */
+
+// Solution for Majority Element
+// Solved on LeetCode
