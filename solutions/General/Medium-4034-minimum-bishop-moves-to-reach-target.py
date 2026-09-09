3c1ea9e1a01b3212145f4ade6aa6d0add8362d50
Siddhant Singh Rawat <rawatsiddhant25@gmail.com>
2026-09-09 21:13:34 +0530

    Add accepted solution for #4034: Minimum Bishop Moves to Reach Target [Medium]

---
+++ solutions/General/Medium-4034-minimum-bishop-moves-to-reach-target.py
@@ -0,0 +1,9 @@
+/*
+ * Problem: 4034. Minimum Bishop Moves to Reach Target
+ * Difficulty: Medium
+ * Topics: 
+ * URL: https://leetcode.com/problems/minimum-bishop-moves-to-reach-target/
+ * Solved via CodePath Auto-Committer
+ */
+
+// Code awaiting input or session cookie
