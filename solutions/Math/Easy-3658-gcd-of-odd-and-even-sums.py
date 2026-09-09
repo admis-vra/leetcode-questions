2ec3e0e378699ffa56870ef4af3505fc22b4201a
Siddhant Singh Rawat <rawatsiddhant25@gmail.com>
2026-09-09 21:13:59 +0530

    Add accepted solution for #3658: GCD of Odd and Even Sums [Easy]

---
+++ solutions/Math/Easy-3658-gcd-of-odd-and-even-sums.py
@@ -0,0 +1,9 @@
+/*
+ * Problem: 3658. GCD of Odd and Even Sums
+ * Difficulty: Easy
+ * Topics: Math, Number Theory
+ * URL: https://leetcode.com/problems/gcd-of-odd-and-even-sums/
+ * Solved via CodePath Auto-Committer
+ */
+
+// Code awaiting input or session cookie
