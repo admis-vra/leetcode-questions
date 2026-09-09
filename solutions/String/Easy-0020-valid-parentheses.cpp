2ab61c6ab8db89a270562920b982fd3dcf314bbf
Siddhant Singh Rawat <rawatsiddhant25@gmail.com>
2026-09-09 21:13:13 +0530

    Add accepted solution for #20: Valid Parentheses [Easy]

--- solutions/String/Easy-0020-valid-parentheses.cpp
+++ solutions/String/Easy-0020-valid-parentheses.cpp
@@ -3,22 +3,7 @@
  * Difficulty: Easy
  * Topics: String, Stack, Bracket Sequences
  * URL: https://leetcode.com/problems/valid-parentheses/
- * Runtime: 0 ms
- * Memory: 7.4 MB
  * Solved via CodePath Auto-Committer
  */
 
-class Solution {
-public:
-    bool isValid(string s) {
-        stack<char> st;
-        for (char c : s) {
-            if (c == '(') st.push(')');
-            else if (c == '{') st.push('}');
-            else if (c == '[') st.push(']');
-            else if (st.empty() || st.top() != c) return false;
-            else st.pop();
-        }
-        return st.empty();
-    }
-};
+// Code awaiting input or session cookie
