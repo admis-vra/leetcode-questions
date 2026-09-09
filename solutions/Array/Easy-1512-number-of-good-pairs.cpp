7db767e73d6605c6296c5c128a955f0fe02ae7a5
Siddhant Singh Rawat <rawatsiddhant25@gmail.com>
2026-09-09 21:31:18 +0530

    Add accepted solution for #1512: Number of Good Pairs [Easy]

---
+++ solutions/Array/Easy-1512-number-of-good-pairs.cpp
@@ -0,0 +1,17 @@
+/*
+ * Problem: 1512. Number of Good Pairs
+ * Difficulty: Easy
+ * Topics: Array, Hash Table, Math, Counting
+ * URL: https://leetcode.com/problems/number-of-good-pairs/
+ *
+ * Description:
+ * Given an array of integers `nums`, return *the number of **good pairs***.
+ * 
+ * A pair `(i, j)` is called *good* if `nums[i] == nums[j]` and `i` 
+ * 	
+ * - `1
+ * Solved via CodePath Auto-Committer
+ */
+
+// Solution for Number of Good Pairs
+// Solved on LeetCode
