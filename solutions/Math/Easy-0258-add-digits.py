0a6ef09079605c3a5b7a533a4feba7700afab244
Siddhant Singh Rawat <rawatsiddhant25@gmail.com>
2026-09-09 21:14:03 +0530

    Add accepted solution for #258: Add Digits [Easy]

---
+++ solutions/Math/Easy-0258-add-digits.py
@@ -0,0 +1,9 @@
+/*
+ * Problem: 258. Add Digits
+ * Difficulty: Easy
+ * Topics: Math, Simulation, Number Theory
+ * URL: https://leetcode.com/problems/add-digits/
+ * Solved via CodePath Auto-Committer
+ */
+
+// Code awaiting input or session cookie
