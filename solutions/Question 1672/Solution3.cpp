88c0bb2547884b3dddba84d1162325341d08b675
Siddhant Singh Rawat <rawatsiddhant25@gmail.com>
2026-09-09 22:54:22 +0530

    feat(Question 1672): add cpp solution (Solution3.cpp) for Richest Customer Wealth [Easy]

---
+++ solutions/Question 1672/Solution3.cpp
@@ -0,0 +1,23 @@
+/*
+ * Problem: 1672. Richest Customer Wealth
+ * Difficulty: Easy
+ * Topics: Array, Matrix
+ * URL: https://leetcode.com/problems/richest-customer-wealth/
+ * Runtime: 0 ms
+ * Memory: 11.3 MB
+ * Solved via CodePath Auto-Committer
+ */
+
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int richest= 0;
+        for (const auto& row : accounts) {
+            int sum = accumulate(row.begin(), row.end(), 0) ;
+            if (sum >= richest) richest = sum;
+        }
+        return richest;
+
+        
+    }
+};
