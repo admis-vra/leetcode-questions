ba3e37f94d68b359350b7d9c987c8e69c4abe95a
Siddhant Singh Rawat <rawatsiddhant25@gmail.com>
2026-09-09 23:04:18 +0530

    feat(Question 1929): add python solution (Solution2.py) for Concatenation of Array [Easy]

---
+++ solutions/Question 1929/Solution2.py
@@ -0,0 +1,15 @@
+# Problem: 1929. Concatenation of Array
+# Difficulty: Easy
+# Topics: Array, Simulation
+# URL: https://leetcode.com/problems/concatenation-of-array/
+# Runtime: 0 ms
+# Memory: 12.8 MB
+# Solved via CodePath Auto-Committer
+
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return nums + nums
