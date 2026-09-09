5a8188692fe03924504ae15c79311631fd95f9ca
Siddhant Singh Rawat <rawatsiddhant25@gmail.com>
2026-09-09 22:57:38 +0530

    feat(Question 2824): add python3 solution (Solution2.py) for Count Pairs Whose Sum is Less than Target [Easy]

---
+++ solutions/Question 2824/Solution2.py
@@ -0,0 +1,16 @@
+# Problem: 2824. Count Pairs Whose Sum is Less than Target
+# Difficulty: Easy
+# Topics: Array, Two Pointers, Binary Search, Sorting
+# URL: https://leetcode.com/problems/count-pairs-whose-sum-is-less-than-target/
+# Runtime: 8 ms
+# Memory: 18.1 MB
+# Solved via CodePath Auto-Committer
+
+class Solution:
+    def countPairs(self, nums: List[int], target: int) -> int:
+        n = []
+        for a in range(len(nums)):
+            for b in range(a+1,len(nums)):
+                if nums[a] + nums[b] < target:
+                    n.append([nums[a], nums[b]])
+        return len(n)        
