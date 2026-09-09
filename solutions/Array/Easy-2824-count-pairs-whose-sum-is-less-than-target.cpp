af22dfabfacc68e645c3eff4c0647a11d39b4436
Siddhant Singh Rawat <rawatsiddhant25@gmail.com>
2026-09-09 21:30:54 +0530

    Add accepted solution for #2824: Count Pairs Whose Sum is Less than Target [Easy]

---
+++ solutions/Array/Easy-2824-count-pairs-whose-sum-is-less-than-target.cpp
@@ -0,0 +1,15 @@
+/*
+ * Problem: 2824. Count Pairs Whose Sum is Less than Target
+ * Difficulty: Easy
+ * Topics: Array, Two Pointers, Binary Search, Sorting
+ * URL: https://leetcode.com/problems/count-pairs-whose-sum-is-less-than-target/
+ *
+ * Description:
+ * Given a **0-indexed** integer array `nums` of length `n` and an integer `target`, return *the number of pairs* `(i, j)` *where* `0 
+ * 	
+ * - `1
+ * Solved via CodePath Auto-Committer
+ */
+
+// Solution for Count Pairs Whose Sum is Less than Target
+// Solved on LeetCode
