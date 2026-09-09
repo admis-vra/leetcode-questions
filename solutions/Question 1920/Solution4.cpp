48978a90177b07b36ab9d8c961f42f6cb4066611
Siddhant Singh Rawat <rawatsiddhant25@gmail.com>
2026-09-09 22:55:07 +0530

    feat(Question 1920): add cpp solution (Solution4.cpp) for Build Array from Permutation [Easy]

---
+++ solutions/Question 1920/Solution4.cpp
@@ -0,0 +1,20 @@
+/*
+ * Problem: 1920. Build Array from Permutation
+ * Difficulty: Easy
+ * Topics: Array, Simulation
+ * URL: https://leetcode.com/problems/build-array-from-permutation/
+ * Runtime: 4 ms
+ * Memory: 21 MB
+ * Solved via CodePath Auto-Committer
+ */
+
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        vector<int> ans;
+        for(int x:nums){
+            ans.push_back(nums[x]);
+        }
+        return ans;
+    }
+};
