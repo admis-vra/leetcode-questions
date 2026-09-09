b56afe7d98adcc727ce0beeb463fada2cbdfd026
Siddhant Singh Rawat <rawatsiddhant25@gmail.com>
2026-09-09 21:31:54 +0530

    Add accepted solution for #234: Palindrome Linked List [Easy]

---
+++ solutions/Linked_List/Easy-0234-palindrome-linked-list.cpp
@@ -0,0 +1,41 @@
+/*
+ * Problem: 234. Palindrome Linked List
+ * Difficulty: Easy
+ * Topics: Linked List, Two Pointers, Stack, Recursion
+ * URL: https://leetcode.com/problems/palindrome-linked-list/
+ *
+ * Description:
+ * Given the `head` of a singly linked list, return `true`* if it is a **palindrome** or *`false`* otherwise*.
+ * 
+ *  
+ * 
+ * **Example 1:**
+ * 
+ * ```
+ * **Input:** head = [1,2,2,1]
+ * **Output:** true
+ * ```
+ * 
+ * **Example 2:**
+ * 
+ * ```
+ * **Input:** head = [1,2]
+ * **Output:** false
+ * ```
+ * 
+ *  
+ * 
+ * **Constraints:**
+ * 
+ * 	
+ * - The number of nodes in the list is in the range `[1, 105]`.
+ * 	
+ * - `0 
+ * 
+ *  
+ * **Follow up:** Could you do it in `O(n)` time and `O(1)` space?
+ * Solved via CodePath Auto-Committer
+ */
+
+// Solution for Palindrome Linked List
+// Solved on LeetCode
