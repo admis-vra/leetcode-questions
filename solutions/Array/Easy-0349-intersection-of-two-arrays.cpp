5877f32926a3f3b79bcff841a373d680d19e698e
Siddhant Singh Rawat <rawatsiddhant25@gmail.com>
2026-09-09 21:31:38 +0530

    Add accepted solution for #349: Intersection of Two Arrays [Easy]

---
+++ solutions/Array/Easy-0349-intersection-of-two-arrays.cpp
@@ -0,0 +1,37 @@
+/*
+ * Problem: 349. Intersection of Two Arrays
+ * Difficulty: Easy
+ * Topics: Array, Hash Table, Two Pointers, Binary Search, Sorting
+ * URL: https://leetcode.com/problems/intersection-of-two-arrays/
+ *
+ * Description:
+ * Given two integer arrays `nums1` and `nums2`, return *an array of their intersection*. Each element in the result must be **unique** and you may return the result in **any order**.
+ * 
+ *  
+ * 
+ * **Example 1:**
+ * 
+ * ```
+ * **Input:** nums1 = [1,2,2,1], nums2 = [2,2]
+ * **Output:** [2]
+ * ```
+ * 
+ * **Example 2:**
+ * 
+ * ```
+ * **Input:** nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * **Output:** [9,4]
+ * **Explanation:** [4,9] is also accepted.
+ * ```
+ * 
+ *  
+ * 
+ * **Constraints:**
+ * 
+ * 	
+ * - `1
+ * Solved via CodePath Auto-Committer
+ */
+
+// Solution for Intersection of Two Arrays
+// Solved on LeetCode
