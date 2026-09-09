61b7127e64fae9fd7acc02c56633480343215b66
Siddhant Singh Rawat <rawatsiddhant25@gmail.com>
2026-09-09 21:13:57 +0530

    Add accepted solution for #206: Reverse Linked List [Easy]

---
+++ solutions/Linked_List/Easy-0206-reverse-linked-list.py
@@ -0,0 +1,9 @@
+/*
+ * Problem: 206. Reverse Linked List
+ * Difficulty: Easy
+ * Topics: Linked List, Recursion
+ * URL: https://leetcode.com/problems/reverse-linked-list/
+ * Solved via CodePath Auto-Committer
+ */
+
+// Code awaiting input or session cookie
