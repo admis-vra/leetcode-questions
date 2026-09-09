2bdbf513415fa83d9a3229f067a5f4c7abbf54a0
Siddhant Singh Rawat <rawatsiddhant25@gmail.com>
2026-09-09 21:13:49 +0530

    Add accepted solution for #4008: Minimum Initial Strength to Defeat All Monsters [Medium]

---
+++ solutions/Array/Medium-4008-minimum-initial-strength-to-defeat-all-monsters.py
@@ -0,0 +1,9 @@
+/*
+ * Problem: 4008. Minimum Initial Strength to Defeat All Monsters
+ * Difficulty: Medium
+ * Topics: Array, Binary Search, Greedy, Prefix Sum
+ * URL: https://leetcode.com/problems/minimum-initial-strength-to-defeat-all-monsters/
+ * Solved via CodePath Auto-Committer
+ */
+
+// Code awaiting input or session cookie
