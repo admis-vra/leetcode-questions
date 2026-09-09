de4de61727e0ae1a2cec43ffb265f38c28c2e2b7
Siddhant Singh Rawat <rawatsiddhant25@gmail.com>
2026-09-09 21:33:35 +0530

    Add accepted solution for #3697: Compute Decimal Representation [Easy]

--- solutions/Array/Easy-3697-compute-decimal-representation.cpp
+++ solutions/Array/Easy-3697-compute-decimal-representation.cpp
@@ -3,6 +3,27 @@
  * Difficulty: Easy
  * Topics: Array, Math
  * URL: https://leetcode.com/problems/compute-decimal-representation/
+ *
+ * Description:
+ * You are given a **positive** integer `n`.
+ * 
+ * A positive integer is a **base-10 component** if it is the product of a single digit from 1 to 9 and a non-negative power of 10. For example, 500, 30, and 7 are **base-10 components**, while 537, 102, and 11 are not.
+ * 
+ * Express `n` as a sum of **only** base-10 components, using the **fewest** base-10 components possible.
+ * 
+ * Return an array containing these **base-10 components** in **descending** order.
+ * 
+ *  
+ * 
+ * **Example 1:**
+ * 
+ * **Input:** n = 537
+ * 
+ * **Output:** [500,30,7]
+ * 
+ * **Explanation:**
+ * 
+ * We can express 537 as `500 + 30 + 7`. It is impossible to express 537 as
  * Solved via CodePath Auto-Committer
  */
 
