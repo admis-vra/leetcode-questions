d17160bd2ecc999423776975b993c6ad834a99f5
Siddhant Singh Rawat <rawatsiddhant25@gmail.com>
2026-09-09 21:13:03 +0530

    Add accepted solution for #1470: Shuffle the Array [Easy]

---
+++ solutions/Array/Easy-1470-shuffle-the-array.cpp
@@ -0,0 +1,9 @@
+/*
+ * Problem: 1470. Shuffle the Array
+ * Difficulty: Easy
+ * Topics: Array
+ * URL: https://leetcode.com/problems/shuffle-the-array/
+ * Solved via CodePath Auto-Committer
+ */
+
+// Code awaiting input or session cookie
