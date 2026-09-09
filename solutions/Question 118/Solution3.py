b1b954a0069bfff910810eb12827b2f15fb63738
Siddhant Singh Rawat <rawatsiddhant25@gmail.com>
2026-09-09 23:02:16 +0530

    feat(Question 118): add python3 solution (Solution3.py) for Pascal's Triangle [Easy]

---
+++ solutions/Question 118/Solution3.py
@@ -0,0 +1,21 @@
+# Problem: 118. Pascal's Triangle
+# Difficulty: Easy
+# Topics: Array, Dynamic Programming
+# URL: https://leetcode.com/problems/pascals-triangle/
+# Runtime: 0 ms
+# Memory: 18 MB
+# Solved via CodePath Auto-Committer
+
+
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        triangle = []
+
+        for i in range(numRows):
+            row = [1] * (i + 1)
+            for j in range(1, i):
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+            triangle.append(row)
+
+        return triangle
