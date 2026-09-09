8d33c8a09b2048f071222b0305776b65f85da007
Siddhant Singh Rawat <rawatsiddhant25@gmail.com>
2026-09-09 22:54:02 +0530

    feat(Question 3697): add cpp solution (Solution7.cpp) for Compute Decimal Representation [Easy]

---
+++ solutions/Question 3697/Solution7.cpp
@@ -0,0 +1,29 @@
+/*
+ * Problem: 3697. Compute Decimal Representation
+ * Difficulty: Easy
+ * Topics: Array, Math
+ * URL: https://leetcode.com/problems/compute-decimal-representation/
+ * Runtime: 3 ms
+ * Memory: 11.6 MB
+ * Solved via CodePath Auto-Committer
+ */
+
+class Solution {
+public:
+    vector<int> decimalRepresentation(int n) {
+        long long num = 1;
+        vector<int> ans;
+        while(n>0){
+            int x = n%10;
+            x *= num;
+            num *= 10; // giving error if num is int;
+            if(x != 0)
+                ans.push_back(x);
+            n = n/10;
+        }
+        // reverse(ans.begin(),ans.end()); // we can also just sort reverse;
+        sort(ans.rbegin(),ans.rend());
+        return ans;
+        
+    }
+};
