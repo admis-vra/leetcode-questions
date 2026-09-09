c4009e362099f473128b0cb24cd7d1fe279c6f28
Siddhant Singh Rawat <rawatsiddhant25@gmail.com>
2026-09-09 21:12:56 +0530

    Add accepted solution for #1672: Richest Customer Wealth [Easy]

---
+++ solutions/Array/Easy-1672-richest-customer-wealth.cpp
@@ -0,0 +1,9 @@
+/*
+ * Problem: 1672. Richest Customer Wealth
+ * Difficulty: Easy
+ * Topics: Array, Matrix
+ * URL: https://leetcode.com/problems/richest-customer-wealth/
+ * Solved via CodePath Auto-Committer
+ */
+
+// Code awaiting input or session cookie
