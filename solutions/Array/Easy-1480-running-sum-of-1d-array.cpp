9a7c451c9f49144f8e074b119ace9ec0d53b5374
Siddhant Singh Rawat <rawatsiddhant25@gmail.com>
2026-09-09 21:13:07 +0530

    Add accepted solution for #1480: Running Sum of 1d Array [Easy]

---
+++ solutions/Array/Easy-1480-running-sum-of-1d-array.cpp
@@ -0,0 +1,9 @@
+/*
+ * Problem: 1480. Running Sum of 1d Array
+ * Difficulty: Easy
+ * Topics: Array, Prefix Sum
+ * URL: https://leetcode.com/problems/running-sum-of-1d-array/
+ * Solved via CodePath Auto-Committer
+ */
+
+// Code awaiting input or session cookie
