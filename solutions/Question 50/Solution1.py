6835e4e10a8111443c98cecba586768a103bfb6e
Siddhant Singh Rawat <rawatsiddhant25@gmail.com>
2026-09-09 23:00:37 +0530

    feat(Question 50): add python3 solution (Solution1.py) for Pow(x, n) [Medium]

---
+++ solutions/Question 50/Solution1.py
@@ -0,0 +1,14 @@
+# Problem: 50. Pow(x, n)
+# Difficulty: Medium
+# Topics: Math, Recursion
+# URL: https://leetcode.com/problems/powx-n/
+# Runtime: 0 ms
+# Memory: 17.8 MB
+# Solved via CodePath Auto-Committer
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return x ** n
+
+obj = Solution()
+print(obj.myPow(2.00000, 10))
